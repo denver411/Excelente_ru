ed656c7fa22963ad17c524e597a0bb5a6d1359aa
Denis Zhebov <zhebov@gmail.com>
2018-06-05 22:12:14 +0300

    Сделал черновик раскрывающегося меню на десктопе

--- js/header.js
+++ js/header.js
@@ -1,9 +1,31 @@
-let topmenu = document.querySelector('.hidden-menu')
+let hiddenMenu = document.querySelector('.hidden-menu');
+let mainMenu = document.querySelector('.menu-block');
+let scrollForFixedMenu = 100;
 
 document.addEventListener('scroll', () => {
-  if (window.pageYOffset >= 100) {
-    topmenu.classList.add('hidden-menu_visible')
+  if (window.pageYOffset >= scrollForFixedMenu) {
+    hiddenMenu.classList.add('hidden-menu_visible');
   } else {
-    topmenu.classList.remove('hidden-menu_visible')
+    hiddenMenu.classList.remove('hidden-menu_visible');
   }
 })
+
+let hiddenMenuButton = document.querySelector('.hidden-menu__menu');
+let hiddenMenuButtonClose = document.querySelector('.hidden-menu__close');
+
+hiddenMenuButton.onclick = function () {
+  if (document.querySelector('.menu-block_from-hidden')) {
+    document.querySelector('.menu-block_from-hidden').classList.toggle('menu-block_from-hidden');
+    hiddenMenuButton.style = '';
+    hiddenMenuButtonClose.style.display = 'none';
+    scrollForFixedMenu = 100;
+  } else {
+    document.querySelector('.menu-block').classList.toggle('menu-block_from-hidden');
+    hiddenMenuButton.style.color = '#ffffff'
+    hiddenMenuButton.style.backgroundImage = 'unset';
+    hiddenMenuButton.style.border = 'solid 2px #ffffff';
+    hiddenMenuButtonClose.style.display = 'flex';
+    scrollForFixedMenu = 0;
+  }
+
+}
