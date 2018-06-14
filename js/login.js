4c076dc01be080604a4c39c799836cd551c55fbb
Aleksandr Berezin <besrezen90@mail.ru>
2018-06-15 00:12:13 +0300

    Мобильный вид страницы регистрации

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+//popup меню регистрации
 let topBlockItemLogin = document.querySelector('.top-block__item_login'),
     login = document.querySelector('.login'),
     loginContentClose = document.querySelector('.login-content__close');
@@ -11,5 +12,18 @@ loginContentClose.onclick = function () {
     if (login.style.display == "none") login.style.display = "block"
     else login.style.display = "none"
 }
+//мобильная версия
 
+let contentLoginMobileSign = document.querySelector('.content-login-mobile__sign'),
+    contentLoginMobileLogin = document.querySelector('.content-login-mobile__login'),
+    formSign = document.querySelector('.form-sign'),
+    formLog = document.querySelector('.form-log');
 
+contentLoginMobileSign.onclick = function () {
+    formSign.style.display = "flex"
+    formLog.style.display = "none"
+}
+contentLoginMobileLogin.onclick = function () {
+    formSign.style.display = "none"
+    formLog.style.display = "flex"
+}
