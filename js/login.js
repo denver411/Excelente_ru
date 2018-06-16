d4cfebb0e9f0c83aa4f3b4a828c25e391f106c1d
Aleksandr Berezin <besrezen90@mail.ru>
2018-06-16 23:16:48 +0300

    Реализована валидация формы popup (c паролем)

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-//popup меню регистрации
+//popup меню регистрации/личного кабинета
 let topBlockItemLogin = document.querySelector('.top-block__item_login'),
     login = document.querySelector('.login'),
     loginContentClose = document.querySelector('.login-content__close'),
@@ -38,4 +38,27 @@ contentLoginMobileSign.onclick = function () {
 contentLoginMobileLogin.onclick = function () {
     formSign.style.display = "none"
     formLog.style.display = "flex"
+}
+
+/* Валидация форм */
+//popup
+popupLogin = document.querySelector('.login-content-ulist-list__login');
+popupLogin.onclick = function () {
+    let loginData = {
+        mail: document.querySelector('.login-content-form__mail-input').value,
+        pass: document.querySelector('.login-content-form__pass-input').value
+    }
+    if (!(/^[a-zA-Z0-9][a-zA-Z0-9-_\.]*@/).test(loginData.mail)) {
+        document.querySelector('.login-content-form__mail-input').style.background = "#c82e2e24";
+        document.querySelector('.login-content-form__mail-input--error').style.display = "block";
+        return false;
+    } else {
+        document.querySelector('.login-content-form__mail-input').style.background = "#5ac82e24";
+        document.querySelector('.login-content-form__mail-input--error').style.display = "none";
+    }
+    if (!(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])[0-9a-zA-Z!@#$%^&*]{6,}/g).test(loginData.pass)) {
+        document.querySelector('.login-content-form__pass-input').style.background = "#c82e2e24";
+        document.querySelector('.login-content-form__pass-input--error').style.display = "block";
+        return false;
+    }
 }
