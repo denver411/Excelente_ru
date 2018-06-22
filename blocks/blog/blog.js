febdc6cc484260760876bcdb7c066f496afe1912
Denis Zhebov <zhebov@gmail.com>
2018-06-22 03:59:31 +0300

    Почистил стили

--- blocks/blog/blog.js
+++ blocks/blog/blog.js
@@ -2,8 +2,8 @@
 let blogMenu = document.querySelectorAll('.blog__menu-item');
 let blogNews = document.querySelectorAll('.blog__news-list');
 
-blogMenu.forEach(function (item, index){
-  item.onclick = function() {
+blogMenu.forEach(function (item, index) {
+  item.onclick = function () {
     let activeBlogMenu = document.querySelector('.blog__menu-item_active');
     let activeBlogNews = document.querySelector('.blog__news-list_active');
     activeBlogMenu.classList.remove('blog__menu-item_active');
