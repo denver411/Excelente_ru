df5c7876a758d53c5fec677f8fd8ab324a92f315
Aleksandr Berezin <besrezen90@mail.ru>
2018-07-18 23:43:47 +0300

    добавил output в валидацию

--- js/validation.js
+++ js/validation.js
@@ -1,28 +1,25 @@
 'use strict'
 
-let generateError = function (text) {
-    let error = document.createElement('output');
+
+let generateError = function (text, input) {
+    let error =  input.nextElementSibling
     error.className = 'form--error';
     error.textContent = text;
-    return error;
-}
 
+}
 let removeValidation = function (form) {
     let errors = form.querySelectorAll('.form--error');
     for (let i = 0; i < errors.length; i++) {
-        errors[i].remove();
+        errors[i].textContent = '';
     }
 }
 
-function insertAfter(elem, refElem) {
-    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
-}
+
 
 let checkFieldsPresence = function (inputs) {
     for (let i = 0; i < inputs.length; i++) {
         if (!inputs[i].value) {
-            let error = generateError('Поле не может быть пустым');
-            insertAfter(error, inputs[i]);
+            generateError('Поле не может быть пустым', inputs[i]);
         }
     }
 }
@@ -30,8 +27,7 @@ let checkFieldsPresence = function (inputs) {
 let checkMailRegEx = function (input) {
     if (input.value.length === 0) return;
     if (!(/^[\w\d]+[\-\_\.\w\d]+\@+[\w\d]+\.[\w]+$/).test(input.value)) {
-        let error = generateError('Поле заполнено с ошибкой');
-        insertAfter(error, input);
+        generateError('Поле заполнено с ошибкой', input);
     }
 }
 
@@ -39,30 +35,26 @@ let checkMailRegEx = function (input) {
 let checkPasswordMatch = function (input) {
     if (input.value.length === 0) return;
     if (input.value.length < 6) {
-        let error = generateError('Пароль не может быть меньше 6 символов');
-        insertAfter(error, input);
+        generateError('Пароль не может быть меньше 6 символов', input);
     }
 }
 
 let checkName = function(input) {
     if (input.value.length === 0) return;
     if (!(/^(([A-zА-я]+[\s]{1}[A-zА-я]+[\s]{1}[A-zА-я]+)|([A-zА-я]+[\s]{1}[A-zА-я]+)|([A-zА-я]+))$/).test(input.value)) {
-        let error = generateError('Недопустимые символы');
-        insertAfter(error, input);
+        generateError('Недопустимые символы', input);
     }
 }
 let checkPhone = function(input) {
     if (input.value.length === 0) return;
     if (!(/^\+7\s\(\d{3}\)\s\d{3}\-\d{2}-\d{2}$/).test(input.value)) {
-        let error = generateError('Поле заполнено с ошибкой');
-        insertAfter(error, input);
+        generateError('Поле заполнено с ошибкой', input);
     }
 }
-let checkReqpass = function() {
-    if (signReqpass.value.length === 0) return;
-    if (signReqpass.value !== signPass.value) {
-        let error = generateError('Введенные пароли не совпадают');
-        insertAfter(error, signReqpass);
+let checkReqpass = function(inputPas, inputReqPas) {
+    if (inputReqPas.value.length === 0) return;
+    if (inputReqPas.value !== inputPas.value) {
+        generateError('Введенные пароли не совпадают', inputReqPas);
     }
 }
 let reqCheckBox = function(input) {
@@ -70,3 +62,4 @@ let reqCheckBox = function(input) {
         formsSign.querySelector('.content-login-form-sign-reg__check-cust').style.background = '#c82e2e24';
     }
 }
+
